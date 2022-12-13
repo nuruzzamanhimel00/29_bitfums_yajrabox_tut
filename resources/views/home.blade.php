56a2ee68707ae797a323a3960f1e996200a64a6e
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-12-13 22:02:01 +0600

    done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,51 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <table class="table" id="myTable">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user )
+                                <tr>
+                                    <th scope="row">
+                                        {{ $loop->iteration }}
+                                    </th>
+                                    <td>
+                                        {{ $user->name }}
+                                    </td>
+                                    <td>
+                                        {{ $user->email }}
+                                    </td>
+                                    <td>@mdo</td>
+                                </tr>
+                            @endforeach
+
+
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+@push('style')
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+@endpush
+
+@push('script')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready( function () {
+        $('#myTable').DataTable();
+    } );
+</script>
+@endpush
