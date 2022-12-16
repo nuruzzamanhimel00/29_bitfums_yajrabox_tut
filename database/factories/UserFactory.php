77f618f2eebdceb5719f9fc7846340a6a74fca2a
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-12-16 20:22:11 +0600

    seed rendom faker issue solved

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,7 +16,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'roles' => $this->faker->randomDigitNot(),
+            'roles' => $this->faker->numberBetween(1,10),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
